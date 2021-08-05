15336cc33feb0e4b24290724ac17087f315e0566
Alexey Lysenko <abesmon@gmail.com>
2021-08-05 20:30:46 +0300

    Initial Commit

---
+++ Shared/GenartPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenartPlaygroundApp.swift
+//  Shared
+//
+//  Created by Алексей Лысенко on 05.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct GenartPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
